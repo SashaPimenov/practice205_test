6204e0e5dede5656684e2903523fca7d624bc59a
SashaPimenov <sasha.pimenov.2002@inbox.ru>
2025-03-29 12:10:00 +0500

    test4

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+}); 
 
 function askForNumber(prompt) {
   return new Promise((resolve) => {
